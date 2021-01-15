f37db2787299143c47f2d9451505bbe88f8fd316
munraj <munrajgrewal12@gmail.com>
2021-01-15 22:28:26 +0000

    fixed it

--- index.js
+++ index.js
@@ -1,29 +1,62 @@
 let balance = 500.00;
 
-class Withdrawal {
-
-  constructor(amount) {
-    this.amount = amount;
+class Transaction {
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
-
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
   }
-
 }
 
+class Deposit extends Transaction {
+  get getAmount() {
+    return this.amount
+  }
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
 
+class Withdrawal extends Transaction {
+  get getAmount() {
+    return 0 - this.amount;
+  }
+  isAllowed() {
+    return true;
+  }
+}
 
+class Account {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+  get balance() {
+    // Calculate the balance using the transaction objects.
+    let balance = 0;
+    for (let t of this.transactions) {
+    	balance += t.value;
+    }
+    return balance;
+  }
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account("snow-patrol");
 
-t1 = new Withdrawal(50.25);
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
